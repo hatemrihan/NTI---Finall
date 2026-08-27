8ac8c23f554698514b5bf1ea3e1bef353717ff26
ahmeddrezk <ahmedrezkramadan@gmail.com>
2026-08-27 06:39:08 +0300

    Add Manrope font family and app color/styles definitions

---
+++ lib/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const backgroundClr = Color(0xFFF7F2EA);
+  static const bottomBackgroundClr = Color(0xFFFFFCF7);
+  static const primaryClr = Color(0xFF294A3A);
+  static const blackClr = Colors.black;
+  static const grayClr = Color(0xff7B827A);
+  static const whiteClr = Colors.white;
+  static const hintClr = Color(0xff7B827A);
+  static const textClr = Color(0xff1E2521);
+}
