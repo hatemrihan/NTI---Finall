c39a3729835b0db2fb5e589803d689996cde4045
ahmeddrezk <ahmedrezkramadan@gmail.com>
2026-08-28 14:42:44 +0300

    Refactor onboarding screens

--- lib/screens/onBoarding1_screen.dart
+++ lib/screens/onBoarding1_screen.dart
@@ -1,10 +1,9 @@
 import 'package:final_project/app_colors.dart';
-import 'package:final_project/app_styles.dart';
-import 'package:final_project/screens/login_screen.dart';
 import 'package:final_project/screens/onBoarding2_screen.dart';
 import 'package:final_project/widgets/custom_elevated_buttom.dart';
-import 'package:final_project/widgets/custom_text_button.dart';
+import 'package:final_project/widgets/onboarding_img_section.dart';
 import 'package:final_project/widgets/onboarding_page_indicator.dart';
+import 'package:final_project/widgets/onboarding_text_section.dart';
 import 'package:flutter/material.dart';
 
 class Onboarding1Screen extends StatelessWidget {
@@ -18,28 +17,7 @@ class Onboarding1Screen extends StatelessWidget {
         child: Stack(
           fit: StackFit.expand,
           children: [
-            Stack(
-              children: [
-                Image.asset(
-                  'assets/images/onBoarding1_img.png',
-                  width: double.infinity,
-                  height: 400,
-                  fit: BoxFit.cover,
-                ),
-                Positioned(
-                  top: 15,
-                  right: 15,
-                  child: CustomTextButton(
-                    text: "skip",
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()),
-                    ),
-                    textClr: AppColors.grayClr,
-                  ),
-                ),
-              ],
-            ),
+            OnboardingImgSection(imgPath: 'assets/images/onBoarding1_img.png'),
             Positioned(
               top: 370,
               left: 0,
@@ -58,24 +36,10 @@ class Onboarding1Screen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Discover Curated Accessories',
-                      style: AppStyles.style28Bold.copyWith(
-                        color: AppColors.textClr,
-                        fontFamily: "Manrope",
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Every piece in our collection is handpicked for its quality, timelessness, and premium craftsmanship.',
-                      style: AppStyles.style18.copyWith(
-                        color: AppColors.grayClr,
-                        height: 1.5,
-                      ),
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
+                    OnboardingTextSection(
+                      title: 'Discover Curated Accessories',
+                      subTitle:
+                          'Every piece in our collection is handpicked for its quality, timelessness, and premium craftsmanship.',
                     ),
                     Spacer(),
                     OnboardingPageIndicator(currentPage: 0),
