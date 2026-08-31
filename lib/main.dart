98ae6c37ece1affc1d7a5bb1754ddcfe4c502211
Ahmed Elhaddad <alsaida958@gmail.com>
2026-08-31 12:17:10 +0300

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:final_project/screens/cart_screen.dart';
 import 'package:final_project/screens/onboarding1_screen.dart';
+import 'package:final_project/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +16,7 @@ class NEMOStore extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Manrope"),
       debugShowCheckedModeBanner: false,
-      home: Onboarding1Screen(),
+      home: CartScreen(),
     );
   }
 }
