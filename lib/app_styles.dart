9b671c9289e1ef0e65b857b5b4e5a7b46eb6979d
Ahmed Elhaddad <alsaida958@gmail.com>
2026-08-31 12:40:35 +0300

    update Appstyles

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -1,14 +1,56 @@
+import 'package:final_project/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppStyles {
   AppStyles._();
-
+  static const TextStyle style13Regular = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style14Regular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style20ExtraBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w900,
+  );
+  static const TextStyle style13SemiBold = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style11Bold = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style14w300 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    color: Color(0xff7B827A),
+    decoration: TextDecoration.lineThrough,
+  );
+  static const TextStyle style22Bold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style24ExtraBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+  );
+  static const TextStyle style13Bold = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.bold,
+  );
   static const TextStyle style48ExtraBold = TextStyle(
     fontSize: 48,
     fontWeight: FontWeight.w800,
   );
   static const TextStyle style28ExtraBold = TextStyle(
     fontSize: 28,
+    fontWeight: FontWeight.w800,
+  );
+  static const TextStyle style16ExtraBold = TextStyle(
+    fontSize: 16,
     fontWeight: FontWeight.w800,
   );
   static const TextStyle style28Bold = TextStyle(
@@ -43,22 +85,21 @@ class AppStyles {
     fontSize: 14,
     fontWeight: FontWeight.w600,
   );
-  static const TextStyle style11 = TextStyle(fontSize: 11);
-
-  static const TextStyle style12 = TextStyle(fontSize: 12);
-  static const TextStyle style14 = TextStyle(fontSize: 14);
-  static const TextStyle style13 = TextStyle(fontSize: 13);
-  static const TextStyle style18 = TextStyle(fontSize: 15);
-  static const TextStyle style20ExtraBold = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w800,
-  );
-  static const TextStyle style27ExtraBold = TextStyle(
-    fontSize: 27,
-    fontWeight: FontWeight.w800,
+  static const TextStyle style12Medium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
   );
   static const TextStyle style18ExtraBold = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w800,
   );
+
+  static const TextStyle style27ExtraBold = TextStyle(
+    fontSize: 27,
+    fontWeight: FontWeight.w800,
+  );
+  static const TextStyle style12 = TextStyle(fontSize: 12);
+  static const TextStyle style14 = TextStyle(fontSize: 14);
+  static const TextStyle style13 = TextStyle(fontSize: 13);
+  static const TextStyle style18 = TextStyle(fontSize: 15);
 }
