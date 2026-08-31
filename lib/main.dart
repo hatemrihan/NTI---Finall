e43ab142f269bfa7ee7b5358b53df0a774baff18
Hatem Rihan <hatemrihan100@gmail.com>
2026-08-31 18:55:57 +0300

    Fix all 51 errors: add missing colors, styles, widgets, and clean unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:final_project/screens/cart_screen.dart';
-import 'package:final_project/screens/onboarding1_screen.dart';
-import 'package:final_project/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
