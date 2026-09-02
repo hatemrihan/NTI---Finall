44510ad672a2dd5239d0e40050986f41c907fd82
Mostafa Saad <mostafasaadhafez7@gmail.com>
2026-09-02 19:46:23 +0300

    Remove incorrect import for app colors in search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,3 @@
-import 'package:final_project/app_colors.dart';
 import 'package:final_project/core/utils/app_colors.dart';
 import 'package:final_project/widgets/custom_search_text_field.dart';
 import 'package:final_project/widgets/cutom_gridview_home.dart';
