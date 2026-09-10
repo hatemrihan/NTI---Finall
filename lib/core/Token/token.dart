767aac8f748f649b7e533fdb82d699f8293ffc3a
Mostafa Saad <mostafasaadhafez7@gmail.com>
2026-09-10 22:14:51 +0300

    feat: implement token storage and retrieval using FlutterSecureStorage

--- lib/core/Token/token.dart
+++ lib/core/Token/token.dart
@@ -1,1 +1,18 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 String token = '';
+
+const storage = FlutterSecureStorage();
+
+Future<void> saveToken(String value) async {
+  token = value;
+
+  await storage.write(
+    key: 'token',
+    value: value,
+  );
+}
+
+Future<void> loadToken() async {
+  token = await storage.read(key: 'token') ?? '';
+}
--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:final_project/features/home/data/models/product_model.dart';
+import 'package:final_project/core/Token/token.dart';
 
 class HomeRemoteDataSource {
   final Dio dio = Dio();
@@ -28,12 +29,7 @@ class HomeRemoteDataSource {
     try {
       final Response response = await dio.get(
         "https://accessories-eshop.runasp.net/api/categories",
-        options: Options(
-          headers: {
-            "Authorization":
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZjgyYWJjNS1hYWJjLTQ2MTEtMzVhYy0wOGRmMGMzZjg1NmQiLCJqdGkiOiJiM2E4NzRkNi0xODk3LTRiNmQtOWVlNy0yN2E1YjM1Mzk5NWQiLCJlbWFpbCI6Im1vc3RhZmFzYWFkaGFmZXo3QGdtYWlsLmNvbSIsIm5hbWUiOiJzdHJpbmcgc3RyaW5nIiwicm9sZXMiOiIiLCJwaWN0dXJlIjoiIiwiZXhwIjoxNzg4OTQ5MjgzLCJpc3MiOiJlc2hvcC5uZXQiLCJhdWQiOiJlc2hvcC5uZXQifQ.an0qG510_S2a07twkgygxDy3Mnw-fokLTM8deKEWTao",
-          },
-        ),
+        options: Options(headers: {"Authorization": "Bearer $token"}),
       );
       List<dynamic> categories = response.data["categories"] ?? [];
       log(categories.toString());
@@ -49,12 +45,7 @@ class HomeRemoteDataSource {
       final Response response = await dio.post(
         'https://accessories-eshop.runasp.net/api/cart/items',
         data: {'productId': productId, 'quantity': 1},
-        options: Options(
-          headers: {
-            'Authorization':
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMDY5MmE4NS00OGQyLTQ2NzUtY2MwMC0wOGRmMDY5Y2ExNDMiLCJqdGkiOiI4ZjczZmQ0My0zOGE2LTRkYWUtODdjMi1lZjIwOWY1OGZjNWIiLCJlbWFpbCI6ImFsc2FpZGE5NThAZ21haWwuY29tIiwibmFtZSI6IkFobWVkIEVsaGFkZGFkIiwicm9sZXMiOiIiLCJwaWN0dXJlIjoiIiwiZXhwIjoxNzg5MDA0NjEzLCJpc3MiOiJlc2hvcC5uZXQiLCJhdWQiOiJlc2hvcC5uZXQifQ.Or2dv76lMZylvgHVre71vebdKctRwka917K3OrWpMVs',
-          },
-        ),
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
       );
     } on DioException catch (e) {
       log('Error in addToCart: $e');
